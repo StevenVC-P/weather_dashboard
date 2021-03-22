339e62eae25ea4f64848bd9b04827cfbf91292f0
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2021-03-21 21:11:07 -0500

    all hist buttons now consol log name, and data from api calls are now displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,159 @@
 var userInput = $('#userinput');
 var search = $('#search');
 var pastSearch = $('#pastsearch');
-var today =$("today");
-var fiveDay =$("fiveday");
+var today =$('today');
+var aside =$('aside');
+var cityName=$('#cityname');
+var currTemp=$('#currtemp');
+var currHum=$('#currhum');
+var windSpeed=$('#windspeed');
+var uvIndex=$('#uvindex');
+var histContain =$();
+var fiveDay = $('#5day');
 
-var searchArray = JSON.parse(localStorage.getItem("userInput")) || [];
 
-searchArray.forEach(function (x,index){
-    var buttonEl = $('<button>')
-    buttonEl.addClass('apirequest')
-    buttonEl.addText(searchArray[i]) //needs to pull name of city searched
+var searchArray = JSON.parse(localStorage.getItem('userInput')) || [];
+
+search.on('click', function (event){
+    event.preventDefault();
+
+    var userInputVal = userInput.val();
+    searchArray.push(userInputVal);
+
+    console.log(searchArray);
+    
+    var requestcurrent = `https://api.openweathermap.org/data/2.5/weather?q=${userInputVal}&units=imperial&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+
+            $.when(
+                $.ajax({
+                    url:requestcurrent,
+                    method:'GET',
+                }),
+            )
+            .then(function(requestcurrent){
+                cityName.html(requestcurrent.name);
+                currTemp.html(requestcurrent.main.temp.toFixed(0));
+                console.log((requestcurrent.main.temp));
+                currHum.html(requestcurrent.main.humidity);
+                windSpeed.html(requestcurrent.wind.speed);
+            })
+    var requestfiveday = `https://api.openweathermap.org/data/2.5/forecast?q=${userInputVal}&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+            $.when(
+                $.ajax({
+                    url:requestfiveday,
+                    method:'GET',
+                }),
+            )
+            .then(function(requestfiveday){
+                console.log(requestfiveday);
+                for( var i = 3; i <requestfiveday.list.length; i+=8){
+                    var dayFig = $('<figure>');
+                    dayFig.addClass('col-md-2');
+
+                    var date = $('<p>');
+                    date.html(requestfiveday.list[i].clouds.dt_txt);
+
+                    var temp = $('<p>');
+                    temp.html(requestfiveday.list[i].main.temp);
+
+                    var hum =$('<p>');
+                    hum.html(requestfiveday.list[i].main.humidity);
+
+                    dayFig.append(date);
+                    dayFig.append(temp);
+                    dayFig.append(hum);
+
+                    fiveDay.append(dayFig);
+                    console.log("loop!");
+                }
+
+
+
+
+
+            });
+
+
+    localStorage.setItem("userInput", JSON.stringify(searchArray));
+    createButtons();
+});
+
+function createButtons () {
+    if (searchArray.length > 1) {
+        histContain.remove();
+    }
+
+    histContain =$('<div>')
+    histContain.addClass('container remove')
+    pastSearch.append(histContain)
+
+    searchArray.forEach(function (item,index){
+
+        var buttonEl = $('<button>')
+        buttonEl.addClass('apirequest col-12')
+        buttonEl.text(searchArray[index])
+    
+        histContain.append(buttonEl);
+
+        //needs to pull name of city searched
+    });
+    // console.log(savedArray);
+        // newApiCall(buttonEl);
+};
+createButtons();
+
+
+pastSearch.on('click', '.apirequest', function(item,index){
+
+    console.log($(this).text());
+    var foo = $(this).text();
+
+
+    var pastrequest = `https://api.openweathermap.org/data/2.5/weather?q=${foo}&units=imperal&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+
+            $.when(
+                $.ajax({
+                    url:pastrequest,
+                    method:'GET',
+                }),
+            )
+            .then(function(pastrequest){
+                console.log(pastrequest);
+            });
+
+
+    // console.log(searchArray[index]);
+    // var foo = $(this);
+    // console.log(foo.val());
+
+    // console.log(foo.);
+    // console.log($(this.outerText));
+    // var pastrequest = $(this)
+
+    // var requestcurrent = `https://api.openweathermap.org/data/2.5/weather?q=${userInputVal}&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+
+    //         $.when(
+    //             $.ajax({
+    //                 url:requestcurrent,
+    //                 method:'GET',
+    //             }),
+    //         )
+    //         .then(function(requestcurrent){
+    //             console.log(requestcurrent);
+    //         })
+    // var requestfiveday = `https://api.openweathermap.org/data/2.5/forecast?q=${userInputVal}&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+    //         $.when(
+    //             $.ajax({
+    //                 url:requestfiveday,
+    //                 method:'GET',
+    //             }),
+    //         )
+    //         .then(function(requestfiveday){
+    //             console.log(requestfiveday);
+    //         });
+
+
+
 });
 
 
-//function to search, add call function to add to local storage
-//function to add to local storage call function to build search
-function addLocalStorage (){
-    //located the textbox to save based on the button clicked
-    var cityNameObj = $(this).siblings('userinput');
-    //takes users's input, the custom data attribute of the text area, and stores it into a variable
-    var cityNameIndex = cityNameObj.attr('data-index');
-    //taking the value from the custom data attribute and adding it to the index of the object which the textbox sits as new key value.
-    search[cityNameIndex].actText = cityNameObj.val();
-    console.log(arrayTime);
-
-    //settimeblock array to be an empty array
-    localStorage.setItem("userInput", JSON.stringify(searchArray));
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-// function getApi() {
-//   // fetch request gets a list of all the repos for the node.js organization
-//   var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
-
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data)
-//       //Loop over the data to generate a table, each table row will have a link to the repo url
-//       for (var i = 0; i < data.length; i++) {
-//         // Creating elements, tablerow, tabledata, and anchor
-//         var createTableRow = document.createElement('tr');
-//         var tableData = document.createElement('td');
-//         var link = document.createElement('a');
-
-//         // Setting the text of link and the href of the link
-//         link.textContent = data[i].html_url;
-//         link.href = data[i].html_url;
-
-//         // Appending the link to the tabledata and then appending the tabledata to the tablerow
-//         // The tablerow then gets appended to the tablebody
-//         tableData.appendChild(link);
-//         createTableRow.appendChild(tableData);
-//         tableBody.appendChild(createTableRow);
-//       }
-//     });
-// }
-
-// fetchButton.addEventListener('click', getApi);
