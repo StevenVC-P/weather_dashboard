8f1fdf8da02864bd04e1914c0c7ecaf889d36a89
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2021-03-22 10:08:55 -0500

    added functionaility to past searches to create fresh searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var today = moment().format('l');
 var userInput = $('#userinput');
 var search = $('#search');
 var pastSearch = $('#pastsearch');
@@ -9,8 +10,8 @@ var currHum=$('#currhum');
 var windSpeed=$('#windspeed');
 var uvIndex=$('#uvindex');
 var histContain =$();
-var fiveDay = $('#5day');
-
+var future = $(".future");
+var fiveArray = [];
 
 var searchArray = JSON.parse(localStorage.getItem('userInput')) || [];
 
@@ -19,6 +20,7 @@ search.on('click', function (event){
 
     var userInputVal = userInput.val();
     searchArray.push(userInputVal);
+    fiveArray.push(userInputVal);
 
     console.log(searchArray);
     
@@ -37,7 +39,7 @@ search.on('click', function (event){
                 currHum.html(requestcurrent.main.humidity);
                 windSpeed.html(requestcurrent.wind.speed);
             })
-    var requestfiveday = `https://api.openweathermap.org/data/2.5/forecast?q=${userInputVal}&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+    var requestfiveday = `https://api.openweathermap.org/data/2.5/forecast?q=${userInputVal}&units=imperial&appid=fc13aea726609eb12f7fb876b4af9bf6`;
             $.when(
                 $.ajax({
                     url:requestfiveday,
@@ -45,17 +47,21 @@ search.on('click', function (event){
                 }),
             )
             .then(function(requestfiveday){
-                console.log(requestfiveday);
+                var fiveDay = $('<div>');
+                fiveDay.addClass('row justify-content-end')
+                future.append(fiveDay)
+
                 for( var i = 3; i <requestfiveday.list.length; i+=8){
                     var dayFig = $('<figure>');
                     dayFig.addClass('col-md-2');
 
                     var date = $('<p>');
-                    date.html(requestfiveday.list[i].clouds.dt_txt);
+                    var day =moment().add(i/8,'days').format('l');
+                    date.html(day);
 
                     var temp = $('<p>');
-                    temp.html(requestfiveday.list[i].main.temp);
-
+                    temp.html(requestfiveday.list[i].main.temp.toFixed(0));
+                    
                     var hum =$('<p>');
                     hum.html(requestfiveday.list[i].main.humidity);
 
@@ -64,13 +70,8 @@ search.on('click', function (event){
                     dayFig.append(hum);
 
                     fiveDay.append(dayFig);
-                    console.log("loop!");
                 }
-
-
-
-
-
+                console.log(fiveDay.length);
             });
 
 
@@ -108,7 +109,6 @@ pastSearch.on('click', '.apirequest', function(item,index){
     console.log($(this).text());
     var foo = $(this).text();
 
-
     var pastrequest = `https://api.openweathermap.org/data/2.5/weather?q=${foo}&units=imperal&appid=fc13aea726609eb12f7fb876b4af9bf6`;
 
             $.when(
@@ -118,42 +118,45 @@ pastSearch.on('click', '.apirequest', function(item,index){
                 }),
             )
             .then(function(pastrequest){
-                console.log(pastrequest);
+                cityName.html(pastrequest.name);
+                currTemp.html(pastrequest.main.temp.toFixed(0));
+                console.log((pastrequest.main.temp));
+                currHum.html(pastrequest.main.humidity);
+                windSpeed.html(pastrequest.wind.speed);
             });
 
+    var pastfiveday = `https://api.openweathermap.org/data/2.5/forecast?q=${foo}&units=imperial&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+            $.when(
+                $.ajax({
+                    url:pastfiveday,
+                    method:'GET',
+                }),
+            )
+            .then(function(pastfiveday){
+                var fiveDay = $('<div>');
+                fiveDay.addClass('row justify-content-end')
+                future.append(fiveDay)
+                console.log(pastfiveday);
+                for( var i = 3; i <pastfiveday.list.length; i+=8){
+                    var dayFig = $('<figure>');
+                    dayFig.addClass('col-md-2');
 
-    // console.log(searchArray[index]);
-    // var foo = $(this);
-    // console.log(foo.val());
+                    var date = $('<p>');
+                    var day =moment().add(i/8,'days').format('l');
+                    date.html(day);
 
-    // console.log(foo.);
-    // console.log($(this.outerText));
-    // var pastrequest = $(this)
+                    var temp = $('<p>');
+                    temp.html(pastfiveday.list[i].main.temp.toFixed(0));
+                    
+                    var hum =$('<p>');
+                    hum.html(pastfiveday.list[i].main.humidity);
 
-    // var requestcurrent = `https://api.openweathermap.org/data/2.5/weather?q=${userInputVal}&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+                    dayFig.append(date);
+                    dayFig.append(temp);
+                    dayFig.append(hum);
 
-    //         $.when(
-    //             $.ajax({
-    //                 url:requestcurrent,
-    //                 method:'GET',
-    //             }),
-    //         )
-    //         .then(function(requestcurrent){
-    //             console.log(requestcurrent);
-    //         })
-    // var requestfiveday = `https://api.openweathermap.org/data/2.5/forecast?q=${userInputVal}&appid=fc13aea726609eb12f7fb876b4af9bf6`;
-    //         $.when(
-    //             $.ajax({
-    //                 url:requestfiveday,
-    //                 method:'GET',
-    //             }),
-    //         )
-    //         .then(function(requestfiveday){
-    //             console.log(requestfiveday);
-    //         });
-
-
-
+                    fiveDay.append(dayFig);
+                    console.log("loop!");
+                }
+            });
 });
-
-
