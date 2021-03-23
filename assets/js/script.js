a259cc4f0c89768629ccacac8e6a3c14fcdebc4f
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2021-03-23 18:00:32 -0500

    added weather icons and color ing to UV blocks rather than the letter.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
+//creating variables
 var today = moment().format('l');
 var userInput = $('#userinput');
 var search = $('#search');
 var pastSearch = $('#pastsearch');
-var today =$('today');
 var aside =$('aside');
 var cityName=$('#cityname');
 var currTemp=$('#currtemp');
@@ -10,35 +10,61 @@ var currHum=$('#currhum');
 var windSpeed=$('#windspeed');
 var uvIndex=$('#uvindex');
 var histContain =$();
-var future = $(".future");
-var fiveArray = [];
+var future = $("#future");
+var todayEl = $("#today");
 
 var searchArray = JSON.parse(localStorage.getItem('userInput')) || [];
 
+// function first adds the user's search input to the master array and then being the api requests based on the input.
 search.on('click', function (event){
     event.preventDefault();
 
     var userInputVal = userInput.val();
     searchArray.push(userInputVal);
-    fiveArray.push(userInputVal);
 
     console.log(searchArray);
-    
+    //first api call
     var requestcurrent = `https://api.openweathermap.org/data/2.5/weather?q=${userInputVal}&units=imperial&appid=fc13aea726609eb12f7fb876b4af9bf6`;
 
-            $.when(
-                $.ajax({
-                    url:requestcurrent,
-                    method:'GET',
-                }),
-            )
-            .then(function(requestcurrent){
-                cityName.html(requestcurrent.name);
-                currTemp.html(requestcurrent.main.temp.toFixed(0));
-                console.log((requestcurrent.main.temp));
-                currHum.html(requestcurrent.main.humidity);
-                windSpeed.html(requestcurrent.wind.speed);
-            })
+        $.ajax({
+            url:requestcurrent,
+            method:'GET',
+        }).then(function(response){
+            console.log(response);
+            cityName.text(response.name +" " + today);
+            currTemp.text("Temperature: " + response.main.temp.toFixed(0) + "°F");
+            console.log((response.main.temp));
+            currHum.text("Humiditiy: " + response.main.humidity +"%");
+            windSpeed.text("Wind Speed: " + response.wind.speed + " MPH");
+            var lat =(response.coord.lat);
+            var lon= (response.coord.lon);
+            //this call takes the latituide and longitude of the first call , and after beig put into varabiles uses them for an api call where we can get a UV Index
+            var requestUVI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&units=imperial&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+            uvi (requestUVI)
+
+        });
+        function uvi (requestUVI){
+            $.ajax({
+                url:requestUVI,
+                method:'GET',
+            }).then(function(response){
+                console.log(response);
+                uvIndex.text("");
+                uvIndex.text("UV Index: ")
+                var color = $('<span>')
+                //created the color block based on the UV that is returned
+                if (response.current.uvi < 2){
+                    color.addClass('low');
+                } else if (response.current.uvi <5){
+                    color.addClass('medium');
+                } else {
+                    color.addClass('high');
+                };
+                color.text(response.current.uvi);
+                uvIndex.append(color)
+            });
+        };
+        //2nd API that uses the user input to request data of next 5 day forecast
     var requestfiveday = `https://api.openweathermap.org/data/2.5/forecast?q=${userInputVal}&units=imperial&appid=fc13aea726609eb12f7fb876b4af9bf6`;
             $.when(
                 $.ajax({
@@ -46,85 +72,111 @@ search.on('click', function (event){
                     method:'GET',
                 }),
             )
-            .then(function(requestfiveday){
-                var fiveDay = $('<div>');
-                fiveDay.addClass('row justify-content-end')
+            .then(function(response){
+                console.log(response)
+                future.text("");    
+                var fiveDay = $('<row>');
+                fiveDay.addClass('row')
                 future.append(fiveDay)
-
-                for( var i = 3; i <requestfiveday.list.length; i+=8){
+                //the given array is 40 periods split over 3 hour increments, using that math, we can get the data for noon of each day.
+                for( var i = 3; i <response.list.length; i+=8){
                     var dayFig = $('<figure>');
                     dayFig.addClass('col-md-2');
 
                     var date = $('<p>');
                     var day =moment().add(i/8,'days').format('l');
-                    date.html(day);
+                    date.text(day);
+
+                    var weatherIcon =$('<img>');
+                    weatherIcon.addClass('weather-icon');
+                    weatherIcon.attr('src','http://openweathermap.org/img/wn/'+ response.list[i].weather[0].icon+'.png');
+                    
 
                     var temp = $('<p>');
-                    temp.html(requestfiveday.list[i].main.temp.toFixed(0));
+                    temp.text("Temp: " + response.list[i].main.temp.toFixed(0) + "°F");
                     
                     var hum =$('<p>');
-                    hum.html(requestfiveday.list[i].main.humidity);
+                    hum.text("Humidity: " + response.list[i].main.humidity+"%");
 
                     dayFig.append(date);
+                    dayFig.append(weatherIcon);
                     dayFig.append(temp);
                     dayFig.append(hum);
 
                     fiveDay.append(dayFig);
                 }
-                console.log(fiveDay.length);
             });
 
 
     localStorage.setItem("userInput", JSON.stringify(searchArray));
     createButtons();
 });
-
+//create the buttons based on the length of the master arry from user inputs
 function createButtons () {
     if (searchArray.length > 1) {
         histContain.remove();
     }
 
     histContain =$('<div>')
-    histContain.addClass('container remove')
+    histContain.addClass('container')
     pastSearch.append(histContain)
 
     searchArray.forEach(function (item,index){
 
         var buttonEl = $('<button>')
-        buttonEl.addClass('apirequest col-12')
+        buttonEl.addClass('apirequest col-12 btn btn-outline-secondary')
         buttonEl.text(searchArray[index])
     
         histContain.append(buttonEl);
-
-        //needs to pull name of city searched
     });
-    // console.log(savedArray);
-        // newApiCall(buttonEl);
+
 };
 createButtons();
 
-
+//creates the same search as the user input but on the buttons that represent past searches.  
 pastSearch.on('click', '.apirequest', function(item,index){
 
     console.log($(this).text());
     var foo = $(this).text();
 
-    var pastrequest = `https://api.openweathermap.org/data/2.5/weather?q=${foo}&units=imperal&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+    var pastrequest = `https://api.openweathermap.org/data/2.5/weather?q=${foo}&units=imperial&appid=fc13aea726609eb12f7fb876b4af9bf6`;
 
-            $.when(
-                $.ajax({
-                    url:pastrequest,
-                    method:'GET',
-                }),
-            )
-            .then(function(pastrequest){
-                cityName.html(pastrequest.name);
-                currTemp.html(pastrequest.main.temp.toFixed(0));
-                console.log((pastrequest.main.temp));
-                currHum.html(pastrequest.main.humidity);
-                windSpeed.html(pastrequest.wind.speed);
+            $.ajax({
+                url:pastrequest,
+                method:'GET',
+            }).then(function(response){
+                cityName.text(response.name +" " + today);
+                currTemp.text("Temperature: "+ response.main.temp.toFixed(0)+ "°F");
+                console.log((response.main.temp));
+                currHum.text("Humidity: "+ response.main.humidity+ "%");
+                windSpeed.text("Wind Speed: " +response.wind.speed+ " MPH");
+                var lat =(response.coord.lat);
+                var lon= (response.coord.lon);
+                var pastUVI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&units=imperial&appid=fc13aea726609eb12f7fb876b4af9bf6`;
+                uvi (pastUVI)
+
             });
-
+            function uvi (pastUVI){
+                $.ajax({
+                    url:pastUVI,
+                    method:'GET',
+                }).then(function(response){
+                    console.log(response);
+                    uvIndex.text("");
+                    uvIndex.text("UV Index: ")
+                    var color = $('<span>')
+    
+                    if (response.current.uvi < 2){
+                        color.addClass('low');
+                    } else if (response.current.uvi <5){
+                        color.addClass('medium');
+                    } else {
+                        color.addClass('high');
+                    };
+                    color.text(response.current.uvi);
+                    uvIndex.append(color)
+                });
+            };
     var pastfiveday = `https://api.openweathermap.org/data/2.5/forecast?q=${foo}&units=imperial&appid=fc13aea726609eb12f7fb876b4af9bf6`;
             $.when(
                 $.ajax({
@@ -133,25 +185,31 @@ pastSearch.on('click', '.apirequest', function(item,index){
                 }),
             )
             .then(function(pastfiveday){
-                var fiveDay = $('<div>');
-                fiveDay.addClass('row justify-content-end')
+                future.text("");
+                var fiveDay = $('<row>');
+                fiveDay.addClass('row')
                 future.append(fiveDay)
-                console.log(pastfiveday);
                 for( var i = 3; i <pastfiveday.list.length; i+=8){
                     var dayFig = $('<figure>');
                     dayFig.addClass('col-md-2');
 
                     var date = $('<p>');
                     var day =moment().add(i/8,'days').format('l');
-                    date.html(day);
+                    date.text(day);
+
+                    var weatherIcon =$('<img>');
+                    weatherIcon.addClass('weather-icon');
+                    weatherIcon.attr('src','http://openweathermap.org/img/wn/'+ response.list[i].weather[0].icon+'.png');
+                    
 
                     var temp = $('<p>');
-                    temp.html(pastfiveday.list[i].main.temp.toFixed(0));
+                    temp.text("Temp: "+ pastfiveday.list[i].main.temp.toFixed(0)+ "°F");
                     
                     var hum =$('<p>');
-                    hum.html(pastfiveday.list[i].main.humidity);
+                    hum.text("Humidity: " +pastfiveday.list[i].main.humidity+ "%");
 
                     dayFig.append(date);
+                    dayFig.append(weatherIcon);
                     dayFig.append(temp);
                     dayFig.append(hum);
 
